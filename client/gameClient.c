99d0954a0a6a149dc0385e641631a7712efa3b15
darkBenky <matus.benky@gmail.com>
2026-04-07 04:04:06 +0200

    Implement idRegister for managing object IDs and enhance RequestData handling

--- client/gameClient.c
+++ client/gameClient.c
@@ -43,6 +43,54 @@ typedef struct {
 	requestObject *objects;
 } RequestData;
 
+typedef struct {
+	uint32 *Ids;
+	Object **Objects;
+	uint32 count;
+	uint32 capacity;
+} idRegister;
+
+void idRegister_Init(idRegister *reg, uint32 initialCapacity) {
+	reg->count = 0;
+	reg->capacity = initialCapacity;
+	reg->Ids = initialCapacity ? malloc(initialCapacity * sizeof(uint32)) : NULL;
+	reg->Objects = initialCapacity ? malloc(initialCapacity * sizeof(Object *)) : NULL;
+}
+
+void idRegister_Add(idRegister *reg, uint32 Id, Object *obj) {
+	if (reg->count >= reg->capacity) {
+		reg->capacity = reg->capacity ? reg->capacity * 2 : 4;
+		reg->Ids = realloc(reg->Ids, reg->capacity * sizeof(uint32));
+		reg->Objects = realloc(reg->Objects, reg->capacity * sizeof(Object *));
+	}
+	reg->Ids[reg->count] = Id;
+	reg->Objects[reg->count] = obj;
+	reg->count++;
+}
+
+void idRegister_Remove(idRegister *reg, uint32 Id) {
+	for (uint32 i = 0; i < reg->count; i++) {
+		if (reg->Ids[i] == Id) {
+			reg->Ids[i] = reg->Ids[reg->count - 1];
+			reg->Objects[i] = reg->Objects[reg->count - 1];
+			reg->count--;
+			return;
+		}
+	}
+}
+
+void idRegister_Free(idRegister *reg) {
+	free(reg->Ids);
+	free(reg->Objects);
+	reg->Ids = NULL;
+	reg->Objects = NULL;
+	reg->count = reg->capacity = 0;
+}
+
+static inline void idRegister_Clear(idRegister *reg) {
+	reg->count = 0;
+}
+
 void RequestData_Init(RequestData *r, uint32 initialCap) {
 	r->numOfObjects = 0;
 	r->capacity = initialCap;
@@ -74,6 +122,20 @@ void addObjectToRequestData(RequestData *request, const Object *obj, uint32 Id) 
 	request->numOfObjects++;
 }
 
+void addFromRegistry(RequestData *request, const idRegister *reg, uint32 Id) {
+	for (uint32 i = 0; i < reg->count; i++) {
+		if (reg->Ids[i] == Id) {
+			addObjectToRequestData(request, reg->Objects[i], Id);
+			return;
+		}
+	}
+}
+
+void addAllFromRegistry(RequestData *request, const idRegister *reg) {
+	for (uint32 i = 0; i < reg->count; i++)
+		addObjectToRequestData(request, reg->Objects[i], reg->Ids[i]);
+}
+
 void postObjects(const Client *c, const RequestData *request) {
 	uint32 payloadSize = sizeof(uint32) + request->numOfObjects * sizeof(requestObject);
 	char *payload = malloc(payloadSize);
@@ -96,36 +158,39 @@ int main(void) {
 	MaterialLib matLib;
 	MaterialLib_Init(&matLib, 0);
 
+	idRegister objectRegistry;
+	idRegister_Init(&objectRegistry, 16);
+
 	Client c = {.host = "127.0.0.1", .port = 8080};
 
 	Object *planeF16 = ObjectList_Add(&scene);
 	uint32 f16Id = generateId(MODEL_F16);
 	LoadObj(pathF16, planeF16, &matLib);
-
+	idRegister_Add(&objectRegistry, f16Id, planeF16);
 	RequestData request;
 	RequestData_Init(&request, 8);
-	addObjectToRequestData(&request, planeF16, f16Id);
+	addFromRegistry(&request, &objectRegistry, f16Id);
 	postObjects(&c, &request);
 	RequestData_Reset(&request);
 
 	// update object position
 	planeF16->position.x += 1.0f;
-	addObjectToRequestData(&request, planeF16, f16Id);
+	addFromRegistry(&request, &objectRegistry, f16Id);
 	sleep(1); // simulate time passing
 	postObjects(&c, &request);
-    RequestData_Reset(&request);
+	RequestData_Reset(&request);
 
-    ClientResponse res = clientGet(&c, "some data", strlen("some data") + 1);
-    printf("[client] GET response (%u bytes): %s\n", res.size, res.data ? res.data : "(empty)");
-    hexDump(res.data, res.size);
-    clientFreeResponse(&res);
+	ClientResponse res = clientGet(&c, "some data", strlen("some data") + 1);
+	printf("[client] GET response (%u bytes): %s\n", res.size, res.data ? res.data : "(empty)");
+	hexDump(res.data, res.size);
+	clientFreeResponse(&res);
 
-    sleep(1);
-    res = clientGet(&c, "some data", strlen("some data") + 1);
-    printf("[client] GET response (%u bytes): %s\n", res.size, res.data ? res.data : "(empty)");
-    hexDump(res.data, res.size);
-    clientFreeResponse(&res);
-
+	sleep(1);
+	res = clientGet(&c, "some data", strlen("some data") + 1);
+	printf("[client] GET response (%u bytes): %s\n", res.size, res.data ? res.data : "(empty)");
+	hexDump(res.data, res.size);
+	clientFreeResponse(&res);
 
 	RequestData_Free(&request);
+	idRegister_Free(&objectRegistry);
 }
